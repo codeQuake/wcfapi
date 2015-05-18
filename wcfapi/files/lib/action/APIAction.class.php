e5e60346a5f28e0e23648e7599ea42ec269ee114
Jens Krumsieck <jenso@codequake.de>
2015-05-18 16:14:33 +0200

    basic system is there  need to handle requests now and extending database objects by implementing an interface. also thinking about using auth-token from user table to get access and application handling to provide access based on api keys.

--- wcfapi/files/lib/action/APIAction.class.php
+++ wcfapi/files/lib/action/APIAction.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace api\action;
 use wcf\action\AbstractAjaxAction;
+use wcf\system\exception\AJAXException;
 use wcf\system\request\RouteHandler;
 
 /**
@@ -18,8 +19,24 @@ class APIAction extends AbstractAjaxAction {
 	public function execute() {
 		parent::execute();
 		
+		/**
+		 * @var $routeData array<mixed>
+		 * contains:
+		 * Array ( 
+		 *		[controller] => api 
+		 *		[request] => request string 
+		 *	)
+		 * example:
+		 * URL api/v1/thread/2/post/1447/ => Array ( [controller] => api [request] => thread/2/post/1447 )
+		 */
 		$routeData = RouteHandler::getInstance()->getRouteData();
-		#DEBUG
-		print_r($routeData);
+		$request = $routeData['request'];
+		
+		/**
+		 * get request method
+		 * available methods: GET, PUT, POST, DELETE
+		 */
+		$method = $_SERVER['REQUEST_METHOD'];
+		if (!in_array($method, array('GET', 'POST', 'PUT', 'DELETE'))) throw new AJAXException('unknown request method');
 	}
 }
