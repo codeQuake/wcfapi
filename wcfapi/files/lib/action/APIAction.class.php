f40bd71762729075e443ee4bdbe9479b318a6bae
Jens Krumsieck <jenso@codequake.de>
2015-05-18 16:52:06 +0200

    first try of handling requests let me know your suggestions how to handle requests :P

--- wcfapi/files/lib/action/APIAction.class.php
+++ wcfapi/files/lib/action/APIAction.class.php
@@ -26,11 +26,28 @@ class APIAction extends AbstractAjaxAction {
 		 *		[controller] => api 
 		 *		[request] => request string 
 		 *	)
-		 * example:
-		 * URL api/v1/thread/2/post/1447/ => Array ( [controller] => api [request] => thread/2/post/1447 )
+		 * example: (user comments)
+		 * URL api/v1/user/2/comment/1447/ => Array ( [controller] => api [request] => user/2/comment/1447 )
 		 */
 		$routeData = RouteHandler::getInstance()->getRouteData();
 		$request = $routeData['request'];
+		
+		//split request in parts
+		$splittedRequest = explode('/', $request);
+		$i = 0;
+		$request = array();
+		foreach ($splittedRequest as $part) {
+			if (intval($part) != 0 && $i != 0) {
+				//ah it's an ID of the previous object! *bulb*
+				$i--;
+				$request[$i]['objectID'] = intval($part);
+			}
+			else {
+				$request[$i] = array('object' => $part);
+			}
+			
+			$i++;
+		}
 		
 		/**
 		 * get request method
@@ -38,5 +55,42 @@ class APIAction extends AbstractAjaxAction {
 		 */
 		$method = $_SERVER['REQUEST_METHOD'];
 		if (!in_array($method, array('GET', 'POST', 'PUT', 'DELETE'))) throw new AJAXException('unknown request method');
+		
+		//have to use special classes, extending normal dbos and implementing our interface
+		$classPrefix = 'api\\data\\';
+		
+		//only get request needs the database object, the others the dbo-action class...
+		$classSuffix = '';
+		if ($method != 'GET') $classSuffix = 'Action';
+		
+		$count = count($request);
+		$i = 0;
+		foreach ($request as $part) {
+			$request[$i]['className'] = $classPrefix.$part['object'].'\\'.ucfirst($part['object']);
+			//only the last object needs the action suffix (like editing a comment on an element)
+			if ($i == $count) $request[$i]['className'] .= $classSuffix;
+			
+			//deactivated for debugging
+			//if (!class_exists($request[$i]['className'])) throw new AJAXException("unable to find class '".$request[$i]['className']."'");
+			$i++;
+		}
+		
+		var_dump($request);
+		/**
+		 * results:
+		 * array(2) { 
+		 *	[0]=> array(3) { 
+		 *		["object"]=> string(4) "user" 
+		 *		["objectID"]=> int(1) 
+		 *		["className"]=> string(18) "api\data\user\User" } 
+		 *	[1]=> array(3) { 
+		 *		["object"]=> string(7) "comment" 
+		 *		["objectID"]=> int(2) 
+		 *		["className"]=> string(24) "api\data\comment\Comment" } 
+		 *	}
+		 *
+		 * any suggestions??
+		 */
+		
 	}
 }
